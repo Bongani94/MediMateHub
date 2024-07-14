3041d08899311be016ac7f7dac2a474a91da193f
Bongani Baloyi <bonganibaloyi94@gmail.com>
2024-07-14 23:08:04 +0200

    Updating UI

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -3,22 +3,30 @@ import { Button, Form, Input } from "antd";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import toast from "react-hot-toast";
+import { useDispatch } from "react-redux";
+import { hideLoading, showLoading } from "../redux/alertsSlice";
 
 function Register() {
-
+  const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const onFinish = async (values) => {
     try {
-      const response = await axios.post("http://localhost:8080/api/user/register", values);
+      dispatch(showLoading());
+      const response = await axios.post(
+        "http://localhost:8080/api/user/register",
+        values
+      );
+      dispatch(hideLoading());
       if (response.data.success) {
         toast.success(response.data.message);
-        toast("Redirecting to login page")
-        navigate("/login")
+        toast("Redirecting to login page");
+        navigate("/login");
       } else {
         toast.error(response.data.message);
       }
     } catch (error) {
+      dispatch(hideLoading());
       toast.error("Something went wrong");
     }
   };
